eb79984b5136e6210309b7498b60382124d7455a
Akjol Jaenbai and david zhu <david@>
2018-08-09 16:10:23 -0700

    add check the tpye of the input

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,37 @@
 $(document).ready(function() {
   $("form#Identity").submit(function(event) {
-    var firstSide = parseInt($("input#first").val());
-    var secondSide = parseInt($("input#second").val());
-    var thirdSide = parseInt($("input#third").val());
+    var firstSide = $("input#first").val();
+    var secondSide = $("input#second").val();
+    var thirdSide = $("input#third").val();
     var result;
 
-        if((firstSide+secondSide)<thirdSide ||
-            secondSide+thirdSide<firstSide||
-            firstSide+thirdSide<secondSide||
-            firstSide===0||
-            secondSide===0||
-            thirdSide===0
+    if(isNaN(firstSide) || isNaN(thirdSide) || isNaN(secondSide)){
+       document.write("PLEASE INPUT THE NUMBER");
+    }else if((firstSide+secondSide)<thirdSide ||
+    secondSide+thirdSide<firstSide||
+    firstSide+thirdSide<secondSide||
+    firstSide===0||
+    secondSide===0||
+    thirdSide===0 ) {
+    result = "Not a triangle";
+    } else if (
+       firstSide === secondSide && secondSide === thirdSide){
+       result = "Equilateral";
+    } else if (firstSide === secondSide ||
+              secondSide === thirdSide||
+              firstSide === thirdSide) {
+              result = "Isosceles";
+    }else { result = "Scalene";
+    }
 
-           ){
-             result = "Not a triangle"
-          }else if (
-            firstSide === secondSide && secondSide === thirdSide){
-            result = "Equilateral"
-            }
-              else if (firstSide === secondSide ||
-                      secondSide === thirdSide||
-                       firstSide === thirdSide)
-              {
-              result = "Isosceles"
+    $(".hideanswers").text(result);
+    //  }else  {
+    //    document.write("PLEASE INPUT THE NUMBER");
+    //  }
 
-              }else{
-                result = "Scalene"
-              }
-            $(".hideanswers").text(result);
+
+
+
 
 
     event.preventDefault();
